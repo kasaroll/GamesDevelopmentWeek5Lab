4b6cdae18d37cd4a4a433eb8da110c1d40e26db9
KasraMosahebiMacBookPro <13539541@student.uts.edu.au>
2020-08-30 01:11:53 +1000

    60 percent complete

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    private Transform[] transArray;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transArray = new Transform[2];
+        transArray[0] = GameObject.FindWithTag("Red").transform;
+        transArray[1] = GameObject.FindWithTag("Blue").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
